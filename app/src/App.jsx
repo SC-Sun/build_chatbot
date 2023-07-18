36f2a4e6eb264b181c52c4b90c4530876f6a5424
SC-Sun <shangchisun@gmail.com>
2023-07-18 17:11:19 +0200

    insert api-Key, fetch Message to ChatGPT & insert TypeIndicator

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,15 +7,57 @@ import {
   MessageList,
   Message,
   MessageInput,
+  TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function App() {
+  const [typing, setTyping] = useState(false);
+
   const [messages, setMessages] = useState([
     {
       message: "Hello! I am ChatGPT",
       sender: "ChatGPT",
     },
   ]);
+
+  const processMessageToChatGPT = async (chatMessages) => {
+    let apiMessages = chatMessages.map((mObject) => {
+      let role = "";
+      if (mObject === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return {
+        role: role,
+        content: mObject.message,
+      };
+    });
+    const system = {
+      role: "system",
+      content: "Explain concepts like an expert",
+    };
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [system, ...apiMessages],
+    };
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+      console.log(data.choices[0].message.content)});
+  };
 
   const sendHandler = async (message) => {
     const newMessage = {
@@ -25,6 +67,8 @@ function App() {
     };
     const newMessages = [...messages, newMessage];
     setMessages(newMessages);
+    setTyping(true);
+    await processMessageToChatGPT(newMessages);
   };
 
   return (
@@ -33,7 +77,13 @@ function App() {
         <div className="container">
           <MainContainer>
             <ChatContainer>
-              <MessageList>
+              <MessageList
+                typingIndicator={
+                  typing ? (
+                    <TypingIndicator content="ChatGPT is typing" />
+                  ) : null
+                }
+              >
                 {messages.map((message, index) => {
                   return <Message key={index} model={message} />;
                 })}
